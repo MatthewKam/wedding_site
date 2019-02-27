29fbc3e44eddd53154af1d9ef113148c457e9b89
Matt Kam <mattkam@Matts-MBP.local>
2019-02-27 15:31:19 -0800

    added htacess and minor changes to rsvp and stuff page

--- rsvp-submit.php
+++ rsvp-submit.php
@@ -6,6 +6,25 @@
 	header('Content-Type: text/plain');
 
 	include 'config.php';
+
+	function submitInvite($conn, $first_name, $last_name, $email, $is_going, $has_guest = null, $guest_first = null, $guest_last = null){
+
+		$sql = '
+			INSERT INTO rsvp (first_name, last_name, email, is_going, has_guest, guest_first, guest_last)
+			VALUES (?,?,?,?,?,?,?)
+		';
+	    $stmt = $conn->prepare($sql);
+	    $stmt->execute(array(
+	    	$first_name, 
+	    	$last_name, 
+	    	$email, 
+	    	$is_going, 
+	    	$has_guest, 
+	    	$guest_first,
+	    	$guest_last
+	    ));
+	}
+
     $connection = new PDO("mysql:dbname=$db;host=$host", $username, $password);
 
 	if (!isset($_POST['first_name']))
@@ -20,23 +39,59 @@
 	if (!isset($_GET['step']))
 		die('error');
 	
-    $sql = 'select allowed_guest from invite where first_name = ? and last_name = ?';
+    $sql = '
+	    SELECT allowed_guest 
+	    FROM invite 
+	    WHERE first_name = ? 
+	    AND last_name = ?
+    ';
     $stmt = $connection->prepare($sql);
-    $stmt->execute(array($_POST['first_name'], $_POST['last_name']));
+    $stmt->execute(array(
+    	$_POST['first_name'], 
+    	$_POST['last_name']
+    ));
     $allowed_guest = $stmt->fetch(PDO::FETCH_COLUMN, 0);
     $is_going = isset($_POST['is_going']);
 	$step = $_GET['step'];
 	
-	if ($allowed_guest === '0')
-		die('thanks');
 	
-	if ($allowed_guest === '1' && !$is_going)
+	if ( $allowed_guest === '0' || ($allowed_guest === '1' && !$is_going) ){
+		submitInvite(
+			$connection,
+			$_POST['first_name'], 
+	    	$_POST['last_name'], 
+	    	$_POST['email'],  
+	    	$is_going ? 1 : 0
+	    );
+
 		die('thanks');
+	}
 	
-	if ($allowed_guest === '1' && $step === '1'){
-		die('guest_input');
-	} else if ( $allowed_guest === '1' && $step === '2'){
-		die('thanks');
+	if ( $allowed_guest === '1' && $step === '1' )
+		die('guest_input'); 
+
+	if ( $allowed_guest === '1' && $step === '2' ){
+
+		if (!isset($_POST['guest_first']))
+			die('error');
+	
+		if (!isset($_POST['guest_last']))
+			die('error');
+
+		$going_solo = isset($_POST['going_solo']);
+
+		submitInvite(
+			$connection,
+			$_POST['first_name'], 
+	    	$_POST['last_name'], 
+	    	$_POST['email'],  
+	    	$is_going ? 1 : 0, 
+	    	$going_solo ? 0 : 1,
+	    	$_POST['guest_first'], 
+	    	$_POST['guest_last']
+	    );
+
+    	die('thanks');
 	}
 
 	die('not_found');
